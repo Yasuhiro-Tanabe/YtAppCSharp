44cdf36875c8f9baa4fedfc402cdd1900031ca5d
Yasuhiro Tanabe <yasuhiro.tanabe@gmail.com>
2021-08-24 08:52:16 +0900

    View と ViewModel の関連付け

---
+++ DDLGenerator/DDLGenerator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDLGenerator.ViewModels
+{
+    /// <summary>
+    /// DDLGeneratorのビューモデル
+    /// </summary>
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// プロパティを更新しイベントを発行する
+        /// </summary>
+        /// <typeparam name="T">プロパティの型</typeparam>
+        /// <param name="variable">プロパティの値を格納する変数名</param>
+        /// <param name="value">プロパティの値</param>
+        /// <param name="caller">プロパティ名、省略可。省略した場合は呼び出し元 setter をもつプロパティ名。</param>
+        private void SetProperty<T>(ref T variable, T value, [CallerMemberName] string caller = "")
+        {
+            variable = value;
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(caller));
+        }
+
+        private string _title = "DDL Generator";
+        public string WindowTitle
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+
+    }
+}
