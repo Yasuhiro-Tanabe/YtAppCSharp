2820416acd6247febffe1c439f245d9947448e2a
Yasuhiro Tanabe <yasuhiro.tanabe@gmail.com>
2021-08-24 10:09:42 +0900

    不要な using 句の除去

--- DDLGenerator/DDLGenerator/ViewModels/MainWindowViewModel.cs
+++ DDLGenerator/DDLGenerator/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,7 @@
 ﻿using DDLGenerator.Commands;
 
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace DDLGenerator.ViewModels
