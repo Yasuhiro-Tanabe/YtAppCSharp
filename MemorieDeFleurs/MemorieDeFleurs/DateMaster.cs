e4f600eb327e4d0d89476c69156ce97fad725f26
Yasuhiro Tanabe <yasuhiro.tanabe@gmail.com>
2021-08-25 14:42:05 +0900

    日付マスタへのデータ登録削除機能追加

--- MemorieDeFleurs/MemorieDeFleurs/DateMaster.cs
+++ MemorieDeFleurs/MemorieDeFleurs/DateMaster.cs
@@ -18,7 +18,7 @@ namespace MemorieDeFleurs
             {
                 using (var query = Connection.CreateCommand())
                 {
-                    query.CommandText = $"select min(DATE_INDEX) from DATE_MASTER";
+                    query.CommandText = $"select min(DATE) from DATE_MASTER";
                     using (var result = query.ExecuteReader())
                     {
                         if (result.HasRows && result.Read() && !result.IsDBNull(0))
@@ -55,6 +55,119 @@ namespace MemorieDeFleurs
 
                 }
             }
+        }
+
+        private int DateTimeToDate(DateTime datetime)
+        {
+            return datetime.Year * 10000 + datetime.Month * 100 + datetime.Day;
+        }
+
+        private DateTime DateToDateTime(int date)
+        {
+            return DateTime.Parse($"{date / 10000:0000}-{(date % 10000) / 100}-{date % 100}");
+        }
+
+        public void Fill(int start, int end)
+        {
+            if(start <= FirstDate)
+            {
+                throw new IndexOutOfRangeException($"開始日 {start} 以前の日付は指定できません。");
+            }
+            if(end <= LastDate)
+            {
+                throw new IndexOutOfRangeException($"終了日 {end} までの日付は登録済みです。");
+            }
+            if(start > end)
+            {
+                var tmp = end;
+                end = start;
+                start = tmp;
+            }
+
+            var startDate = DateToDateTime(start);
+            var endDate = DateToDateTime(end);
+            if(start <= LastDate)
+            {
+                var last = DateToDateTime(LastDate);
+                startDate = last.AddDays(1);
+            }
+
+            if(startDate == endDate)
+            {
+                // 何もしない
+                return;
+            }
+
+
+            var transaction = Connection.BeginTransaction();
+
+            try
+            {
+                using(var cmd = Connection.CreateCommand())
+                {
+                    cmd.Transaction = transaction;
+                    cmd.CommandText = "insert into DATE_MASTER values ( @date, @index )";
+
+                    var index = LastDate < 0 ? 0 : DateToIndex(LastDate) + 1;
+                    for (var d = startDate; d <= endDate; d = d.AddDays(1), index++)
+                    {
+                        cmd.Parameters.AddWithValue("@date", DateTimeToDate(d));
+                        cmd.Parameters.AddWithValue("@index", index);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                transaction.Commit();
+            }
+            catch(Exception ex)
+            {
+                transaction.Rollback();
+                throw;
+            }
+        }
+
+        public bool IsValidDate(int date)
+        {
+            DateTime dt;
+            return DateTime.TryParse($"{date / 10000:0000}-{(date % 10000) / 100}-{date % 100}", out dt);
+        }
+
+        private int DateToIndex(int date)
+        {
+            if(date < FirstDate || LastDate < date)
+            {
+                throw new IndexOutOfRangeException($"未登録日付です：{date}");
+            }
+            if(!IsValidDate(date))
+            {
+                throw new IndexOutOfRangeException($"不正な日付です：{date}");
+            }
+
+            using (var cmd = Connection.CreateCommand())
+            {
+                cmd.CommandText = $"select DATE_INDEX from DATE_MASTER where DATE={date}";
+                using (var result = cmd.ExecuteReader())
+                {
+                    if (result.HasRows && result.Read() && !result.IsDBNull(0))
+                    {
+                        return result.GetInt32(0);
+                    }
+                    else
+                    {
+                        return -1;
+                    }
+                }
+
+            }
+        }
+
+        public void Clear()
+        {
+            using(var cmd = Connection.CreateCommand())
+            {
+                cmd.CommandText = "delete from DATE_MASTER";
+                cmd.ExecuteNonQuery();
+            }
+
         }
     }
 }
--- MemorieDeFleurs/MemorieDeFleursTest/DateMasterTest.cs
+++ MemorieDeFleurs/MemorieDeFleursTest/DateMasterTest.cs
@@ -17,10 +17,15 @@ namespace MemorieDeFleursTest
         private SQLiteConnection TestDB { get; set; }
         private SQLiteConnection EmptyDB { get; set; }
 
+        private DateMaster TestDateMaster { get; set; }
+        private DateMaster EmptyDateMaster { get; set; }
+
         public DateMasterTest()
         {
             TestDB = CreateDBConnection(TestDBFile);
             EmptyDB = CreateDBConnection(EmptyDBFile);
+            TestDateMaster = new DateMaster(TestDB);
+            EmptyDateMaster = new DateMaster(EmptyDB);
         }
 
         private SQLiteConnection CreateDBConnection(string dbFileName)
@@ -31,6 +36,7 @@ namespace MemorieDeFleursTest
             builder.DataSource = dbFileName;
             builder.ForeignKeys = true;
             builder.ReadOnly = false;
+            builder.SyncMode = SynchronizationModes.Normal;
 
             LogUtil.Debug($"CreateConnection({dbFileName})=>DataSource={builder.ToString()}");
             return new SQLiteConnection(builder.ToString());
@@ -46,6 +52,7 @@ namespace MemorieDeFleursTest
         [TestCleanup]
         public void TearDown()
         {
+            TestDateMaster.Clear();
             TestDB.Close();
             EmptyDB.Close();
         }
@@ -53,10 +60,44 @@ namespace MemorieDeFleursTest
         [TestMethod]
         public void NoDatesInEmptyDb()
         {
-            var master = new DateMaster(EmptyDB);
-            Assert.AreEqual(-1, master.FirstDate);
-            Assert.AreEqual(-1, master.LastDate);
+            Assert.AreEqual(-1, EmptyDateMaster.FirstDate);
+            Assert.AreEqual(-1, EmptyDateMaster.LastDate);
         }
 
+        [TestMethod]
+        public void AddAndRemoveDateMaster()
+        {
+            var startDate = 20200101;
+            var endDate = 20221231;
+            var invalidDate = -1;
+
+            TestDateMaster.Fill(startDate, endDate);
+            Assert.AreEqual(startDate, TestDateMaster.FirstDate);
+            Assert.AreEqual(endDate, TestDateMaster.LastDate);
+
+            TestDateMaster.Clear();
+            Assert.AreEqual(invalidDate, TestDateMaster.FirstDate);
+            Assert.AreEqual(invalidDate, TestDateMaster.LastDate);
+        }
+
+        [TestMethod]
+        public void IsValidDate()
+        {
+            Assert.IsTrue(EmptyDateMaster.IsValidDate(20200101));
+            Assert.IsTrue(EmptyDateMaster.IsValidDate(20211231));
+            Assert.IsTrue(EmptyDateMaster.IsValidDate(20990630));
+            Assert.IsTrue(EmptyDateMaster.IsValidDate(20200229)); // ‰[”N‚Å‚ ‚é
+            Assert.IsTrue(EmptyDateMaster.IsValidDate(20000229)); // ‰[”N‚Å‚ ‚èA‚©‚Â¼—ï”N%400=0‚È‚Ì‚Å‰[“ú‚ª‚ ‚é
+        }
+
+        [TestMethod]
+        public void IsInvalidDate()
+        {
+            Assert.IsFalse(EmptyDateMaster.IsValidDate(20200100));
+            Assert.IsFalse(EmptyDateMaster.IsValidDate(20211301));
+            Assert.IsFalse(EmptyDateMaster.IsValidDate(20231131));
+            Assert.IsFalse(EmptyDateMaster.IsValidDate(20210229));// ‰[”N‚Å‚Í‚È‚¢
+            Assert.IsFalse(EmptyDateMaster.IsValidDate(21000229));// ‰[”N‚¾‚ª‰[“ú‚ª‚È‚¢(400”N‚É3‰ñA¼—ï”N%400=100,200,300‚Ì”N)
+        }
     }
 }
