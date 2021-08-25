91b6b9c1cd8460a7c9d32842fd55b9c4916769a9
Yasuhiro Tanabe <yasuhiro.tanabe@gmail.com>
2021-08-25 17:09:05 +0900

    SequenceUtil 追加

---
+++ MemorieDeFleurs/MemorieDeFleurs/SequenceUtil.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MemorieDeFleurs
+{
+    /// <summary>
+    /// シーケンス操作ユーティリティ
+    /// </summary>
+    public class SequenceUtil
+    {
+        private const string GetSQL = "select VALUE from SEQUENCES where NAME=@name";
+        private const string SetSQL = "update SEQUENCES set VALUE = @value where NAME=@name";
+        private const string CreateSQL = "insert into SEQUENCES values (@name, @value)";
+        private const string Value = "@value";
+        private const string Name = "@name";
+
+        private SQLiteCommand GetStatement;
+        private SQLiteCommand SetStatement;
+        private SQLiteCommand CreateStatement;
+
+        private SQLiteConnection Connection { get; set; }
+
+        public class SequenceValue
+        {
+            private string Name { get; set; }
+
+            private SequenceUtil Util { get; set; }
+
+            public int Next
+            {
+                get { return Util.Next(Name); }
+            }
+
+            public SequenceValue(string name, SequenceUtil parent)
+            {
+                Name = name;
+                Util = parent;
+            }
+        }
+
+        /// <summary>
+        /// シーケンス SEQ_CUSTOMERS
+        /// 
+        /// SEQ_CUSTOMERS.Next で次の連番を自動採番・取得できる。
+        /// </summary>
+        public SequenceValue SEQ_CUSTOMERS { get; private set; }
+
+        /// <summary>
+        /// シーケンス SEQ_SHIPPING
+        /// 
+        /// SEQ_SHIPPING.Next で次の連番を自動採番・取得できる。
+        /// </summary>
+        public SequenceValue SEQ_SHIPPING { get; private set; }
+
+        /// <summary>
+        /// シーケンス SEQ_SUPPLIERS
+        /// 
+        /// SEQ_SUPPLIERS.Next で次の連番を自動採番・取得できる。
+        /// </summary>
+        public SequenceValue SEQ_SUPPLIERS { get; private set; }
+
+        /// <summary>
+        /// シーケンス SEQ_SESSION
+        /// 
+        /// SEQ_SESSION.Next で次の連番を自動採番・取得できる。
+        /// </summary>
+        public SequenceValue SEQ_SESSION { get; private set; }
+
+        /// <summary>
+        /// コンストラクタ。
+        /// 
+        /// テストでの使い勝手をよくするため、DBとの接続は外部から注入させる。
+        /// </summary>
+        /// <param name="conn">接続先DB</param>
+        public SequenceUtil(SQLiteConnection conn)
+        {
+            Connection = conn;
+
+            GetStatement = conn.CreateCommand();
+            GetStatement.CommandText = GetSQL;
+            GetStatement.Parameters.AddWithValue(Name, ""); ;
+
+            SetStatement = conn.CreateCommand();
+            SetStatement.CommandText = SetSQL;
+            SetStatement.Parameters.Add(Name, DbType.String);
+            SetStatement.Parameters.Add(Value, DbType.Int32);
+
+            CreateStatement = conn.CreateCommand();
+            CreateStatement.CommandText = CreateSQL;
+            CreateStatement.Parameters.Add(Name, DbType.String);
+            CreateStatement.Parameters.Add(Value, DbType.Int32);
+
+            SEQ_CUSTOMERS = new SequenceValue("SEQ_CUSTOMERS", this);
+            SEQ_SHIPPING = new SequenceValue("SEQ_SHIPPING", this);
+            SEQ_SUPPLIERS = new SequenceValue("SEQ_SUPPLIERS", this);
+            SEQ_SESSION = new SequenceValue("SEQ_SESSION", this);
+        }
+
+        private int Next(string sequenceName)
+        {
+            var transaction = Connection.BeginTransaction();
+
+            try
+            {
+                GetStatement.Transaction = transaction;
+                SetStatement.Transaction = transaction;
+                CreateStatement.Transaction = transaction;
+
+                GetStatement.Parameters[Name].Value = sequenceName;
+
+                var next = 0;
+                using (var reader = GetStatement.ExecuteReader())
+                {
+                    if (reader.HasRows && reader.Read() && !reader.IsDBNull(0))
+                    {
+                        next = reader.GetInt32(0) + 1;
+
+                        SetStatement.Parameters[Name].Value = sequenceName;
+                        SetStatement.Parameters[Value].Value = next;
+                        SetStatement.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        next = 1;
+
+                        CreateStatement.Parameters[Name].Value = sequenceName;
+                        CreateStatement.Parameters[Value].Value = next;
+                        CreateStatement.ExecuteNonQuery();
+                    }
+                }
+
+                transaction.Commit();
+
+                return next;
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+                throw;
+            }
+        }
+
+    }
+}
---
+++ MemorieDeFleurs/MemorieDeFleursTest/SequenceUtilTest.cs
@@ -0,0 +1,80 @@
+﻿using MemorieDeFleurs;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemorieDeFleursTest
+{
+    [TestClass]
+    public class SequenceUtilTest
+    {
+        private static string TestDBFile = "./testdata/db/MemorieDeFleurs.db";
+
+        private SQLiteConnection TestDB { get; set; }
+
+        private SequenceUtil TestSequence { get; set; }
+
+
+        public SequenceUtilTest()
+        {
+            TestDB = CreateDBConnection(TestDBFile);
+            TestSequence = new SequenceUtil(TestDB);
+        }
+
+        private SQLiteConnection CreateDBConnection(string dbFileName)
+        {
+            var builder = new SQLiteConnectionStringBuilder();
+
+            builder.SetDefaults = true;
+            builder.DataSource = dbFileName;
+            builder.ForeignKeys = true;
+            builder.ReadOnly = false;
+            builder.SyncMode = SynchronizationModes.Normal;
+
+            LogUtil.Debug($"CreateConnection({dbFileName})=>DataSource={builder.ToString()}");
+            return new SQLiteConnection(builder.ToString());
+        }
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            TestDB.Open();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            TestDB.Close();
+        }
+
+        [TestMethod]
+        public void GetSeqCostmoersFirstValue()
+        {
+            Assert.AreEqual(1, TestSequence.SEQ_CUSTOMERS.Next);
+        }
+
+        [TestMethod]
+        public void GetSeqCostomersNextValue()
+        {
+            Assert.AreEqual(2, TestSequence.SEQ_CUSTOMERS.Next);
+        }
+
+        [TestMethod]
+        public void GetSeqShippingFirstValue()
+        {
+            Assert.AreEqual(1, TestSequence.SEQ_SHIPPING.Next);
+        }
+
+        [TestMethod]
+        public void GetSeqShippingNextValue()
+        {
+            Assert.AreEqual(2, TestSequence.SEQ_SHIPPING.Next);
+        }
+    }
+}
