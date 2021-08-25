d4837943e8254a90db09d6cda4bd42db36b547da
Yasuhiro Tanabe <yasuhiro.tanabe@gmail.com>
2021-08-25 18:33:21 +0900

    TDD演習用のプロジェクトを新規作成

---
+++ GameOfLife/GameOfLife.Core.Test/FieldTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLife.Core.Test
+{
+    [TestClass]
+    public class FieldTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
