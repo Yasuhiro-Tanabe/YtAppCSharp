defb4fd8331a54959995cf3d4378f742f4c50c89
Yasuhiro Tanabe <yasuhiro.tanabe@gmail.com>
2021-08-30 14:59:19 +0900

    [MemorieDeFleurs] 得意先データモデル追加

---
+++ MemorieDeFleurs/MemorieDeFleurs/Models/CustomerModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemorieDeFleurs.Models
+{
+    public class CustomerModel
+    {
+        private MemorieDeFleursDbContext DbContext { get; set; }
+        private MemorieDeFleursModel Parent { get; set; }
+
+        private SequenceUtil Sequences { get { return Parent.Sequences; } }
+
+        /// <summary>
+        /// (パッケージ内限定)コンストラクタ
+        /// 
+        /// モデルのプロパティとして参照できるので、外部でこのオブジェクトを作成することは想定しない。
+        /// </summary>
+        /// <param name="parent"></param>
+        internal CustomerModel(MemorieDeFleursModel parent)
+        {
+            Parent = parent;
+            DbContext = parent.DbContext;
+        }
+    }
+}
