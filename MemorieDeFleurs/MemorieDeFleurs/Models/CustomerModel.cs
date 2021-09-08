a7cf65ba62a664f57351bc7381489506036111dc
Yasuhiro Tanabe <yasuhiro.tanabe@gmail.com>
2021-09-08 10:41:57 +0900

    [MemorieDeFleurs] 仕入先ビルダとビルダを使ったテストの追加

--- MemorieDeFleurs/MemorieDeFleurs/Models/CustomerModel.cs
+++ MemorieDeFleurs/MemorieDeFleurs/Models/CustomerModel.cs
@@ -1,9 +1,18 @@
-﻿namespace MemorieDeFleurs.Models
+﻿using MemorieDeFleurs.Entities;
+
+using System.Linq;
+
+namespace MemorieDeFleurs.Models
 {
     public class CustomerModel
     {
         private MemorieDeFleursDbContext DbContext { get; set; }
         private MemorieDeFleursModel Parent { get; set; }
+
+        private int NextCustomerID
+        {
+            get { return Parent.Sequences.SEQ_CUSTOMERS.Next; }
+        }
 
         /// <summary>
         /// (パッケージ内限定)コンストラクタ
@@ -16,5 +25,114 @@
             Parent = parent;
             DbContext = parent.DbContext;
         }
+
+        #region CustomerBuilder
+        /// <summary>
+        /// 得意先オブジェクト生成器
+        /// 
+        /// 各プロパティはフルーエントインタフェースで入力し、最後に Create() で生成とデータベース登録を行う。
+        /// </summary>
+        public class CustomerBuilder
+        {
+            private CustomerModel _model;
+
+            string _emailAddress;
+            string _name;
+            string _password;
+            string _cardNo;
+
+            internal static CustomerBuilder GetInstance(CustomerModel parent)
+            {
+                return new CustomerBuilder(parent);   
+            }
+
+            private CustomerBuilder(CustomerModel model)
+            {
+                _model = model;
+            }
+
+            private MemorieDeFleursDbContext DbContext { get { return _model.DbContext; } }
+
+            /// <summary>
+            /// 得意先名称を登録/変更する
+            /// </summary>
+            /// <param name="name">得意先名称</param>
+            /// <returns>得意先名称変更後の得意先オブジェクト生成器(自分自身)</returns>
+            public CustomerBuilder NameIs(string name)
+            {
+                _name = name;
+                return this;
+            }
+
+            /// <summary>
+            /// e-メールアドレスを登録/変更する
+            /// </summary>
+            /// <param name="address">e-メールアドレス</param>
+            /// <returns>e-メールアドレス変更後の得意先オブジェクト生成器(自分自身)</returns>
+            public CustomerBuilder EmailAddressIs(string address)
+            {
+                _emailAddress = address;
+                return this;
+            }
+
+            /// <summary>
+            /// パスワードを登録/変更する
+            /// </summary>
+            /// <param name="passwd">パスワード</param>
+            /// <returns>パスワード変更後の得意先オブジェクト生成器(自分自身)</returns>
+            public CustomerBuilder PasswordIs(string passwd)
+            {
+                _password = passwd;
+                return this;
+            }
+
+            /// <summary>
+            /// キャッシュカード番号を登録/変更する
+            /// </summary>
+            /// <param name="no">キャッシュカード番号</param>
+            /// <returns>キャッシュカード番号変更後の得意先オブジェクト生成器(自分自身)</returns>
+            public CustomerBuilder CardNoIs(string no)
+            {
+                _cardNo = no;
+                return this;
+            }
+
+            public Customer Create()
+            {
+                var c = new Customer()
+                {
+                    ID = _model.NextCustomerID,
+                    Name = _name,
+                    EmailAddress = _emailAddress,
+                    Password = _password,
+                    CardNo = _cardNo,
+                    Status = 0
+                };
+
+                DbContext.Customers.Add(c);
+                DbContext.SaveChanges();
+
+                return c;
+            }
+        }
+
+        public CustomerBuilder Entity<Customer>()
+        {
+            return CustomerBuilder.GetInstance(this);
+        }
+        #endregion // CustomerBuilder
+
+        #region 仕入先の登録改廃
+        public Customer Find(int id)
+        {
+            return DbContext.Customers.SingleOrDefault(c => c.ID == id);
+        }
+        #endregion // 仕入先の登録改廃
+
+        #region 注文
+        #endregion // 注文
+
+        #region 注文取消
+        #endregion // 注文取消
     }
 }
---
+++ MemorieDeFleurs/MemorieDeFleursTest/ModelTest/CustomerModelTest.cs
@@ -0,0 +1,59 @@
+﻿using MemorieDeFleurs.Entities;
+using MemorieDeFleurs.Models;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemorieDeFleursTest.ModelTest
+{
+    [TestClass]
+    public class CustomerModelTest : MemorieDeFleursDbContextTestBase
+    {
+        private MemorieDeFleursModel Model { get; set; }
+
+        public CustomerModelTest() : base()
+        {
+            AfterTestBaseInitializing += PrepareModel;
+            BeforeTestBaseCleaningUp += CleanupModel;
+        }
+
+        #region TestInitialize
+        public void PrepareModel(object sender, EventArgs unused)
+        {
+            Model = new MemorieDeFleursModel(TestDBContext);
+        }
+        #endregion // TestInitialize
+
+        #region TestTerminate
+        public void CleanupModel(object sender, EventArgs unused)
+        {
+            ClearAll();
+        }
+        #endregion // TestTerminate
+
+        [TestMethod]
+        public void CanAddCustomerViaModel()
+        {
+            var model = new MemorieDeFleursModel(TestDBContext);
+
+            var expected = model.CustomerModel.Entity<Customer>()
+                .NameIs("蘇我幸恵")
+                .PasswordIs("sogayukie12345")
+                .EmailAddressIs("ysoga@localdomain")
+                .CardNoIs("9876543210123210")
+                .Create();
+
+            var actual = model.CustomerModel.Find(expected.ID);
+
+            Assert.AreEqual(expected.ID, actual.ID);
+            Assert.AreEqual(expected.Name, actual.Name);
+            Assert.AreEqual(expected.EmailAddress, actual.EmailAddress);
+            Assert.AreEqual(expected.Password, actual.Password);
+        }
+    }
+}
