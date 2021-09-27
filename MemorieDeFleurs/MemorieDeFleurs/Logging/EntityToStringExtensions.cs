e853ff5d53edfb388d35eeb69342ff72da25012d
Yasuhiro Tanabe <yasuhiro.tanabe@gmail.com>
2021-09-27 18:01:53 +0900

    [MemorieDeFleurs] 在庫アクション出力書式に "o" を追加

--- MemorieDeFleurs/MemorieDeFleurs/Logging/EntityToStringExtensions.cs
+++ MemorieDeFleurs/MemorieDeFleurs/Logging/EntityToStringExtensions.cs
@@ -10,17 +10,64 @@ namespace MemorieDeFleurs.Logging
 {
     public static class EntityToStringExtensions
     {
+        /// <summary>
+        /// 在庫アクションを特定の書式で文字列化する
+        /// 
+        /// 書式毎の出力内容は下記。
+        /// <code>
+        /// var action = new InventoryAction() {
+        ///     InventoryActionType.SCHEDULED_TO_USE,
+        ///     ActionDate = new DateTime(2020, 5, 3),
+        ///     ArrivalDate = new DateTime(2020, 5, 5),
+        ///     InventoryLotNo = 1,
+        ///     PartsCode = "BA001",
+        ///     Quantity = 0,
+        ///     Remain = 100
+        /// };
+        /// 
+        /// action.ToString("s");  // BA001.Lot1[20200505](Q=0,R=100)
+        /// action.ToString("L");  // SCHEDULED_TO_USE[Parts=BA001, ArrivalDate=20200503, Lot=1, ActionDate=20200505, Quality=0, Remain=100]
+        /// action.ToString("h");  // BA001.Lot1[20200505]
+        /// action.ToString("o");  // Lot1, BA001 x 0, arrive at 20200503
+        /// action.ToString("x");  // s, L, h, o 以外の書式を指定した場合、ArguemtException がスローされる
+        /// action.ToString(" ");  // 空白類を指定した場合も、上記同様 ArguemtException がスローされる
+        /// action.ToString("");   // ArgumentNullException がスローされる
+        /// action.ToString();     // ArgumentNullException がスローされる
+        /// action.ToString(null); // ArgumentNullException がスローされる
+        /// </code>
+        /// 
+        /// <list type="">
+        ///     <item>
+        ///         <term>"s"</term>
+        ///         <description>短い書式：花コード、ロット番号、基準日、数量、残数を含む文字列</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>"L"</term>
+        ///         <description>長い書式：短い書式の出力内容に加えて、在庫アクションタイプと納品日(納品予定日)を含む文字列</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>"h"</term>
+        ///         <description>ヘッダー書式：花コード、ロット番号、基準日のみを含む文字列</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>"o"</term>
+        ///         <description>発注パラメータ書式：花コード、ロット番号、数量、納品予定日のみを含む文字列</description>
+        ///     </item>
+        /// </list>
+        /// </summary>
+        /// <param name="action">対象在庫アクション</param>
+        /// <param name="format">書式：s,L,h,o のいずれか。各書式は説明欄参照。</param>
+        /// <returns>書式に従って在庫アクションを出力した文字列</returns>
         public static string ToString(this InventoryAction action, string format = "")
         {
-            if(string.IsNullOrWhiteSpace(format))
+            if(string.IsNullOrEmpty(format))
             {
-                return action.ToString();
+                throw new ArgumentNullException("null or white space is not supported.");
             }
             else if(format == "s")
             {
                 return new StringBuilder()
-                    .Append(action.PartsCode)
-                    .AppendFormat(".Lot{0}[{1:yyyyMMdd}]", action.InventoryLotNo, action.ActionDate)
+                    .Append(action.ToString("h"))
                     .AppendFormat("(Q={0},R={1})", action.Quantity, action.Remain)
                     .ToString();
             }
@@ -40,9 +87,17 @@ namespace MemorieDeFleurs.Logging
             {
                 return $"{action.PartsCode}.Lot{action.InventoryLotNo}[{action.ActionDate.ToString("yyyyMMdd")}]";
             }
+            else if(format == "o")
+            {
+                return new StringBuilder()
+                    .AppendFormat("Lot{0}", action.InventoryLotNo)
+                    .AppendFormat(", {0} x {1}", action.PartsCode, action.Quantity)
+                    .AppendFormat(", arrive at {0:yyyyMMdd}", action.ArrivalDate)
+                    .ToString();
+            }
             else
             {
-                return action.ToString();
+                throw new ArgumentException($"Invalid argument: {format}");
             }
         }
     }
