eaeefa2c327bcb742c72b0e825088c7603b5e136
Yasuhiro Tanabe <yasuhiro.tanabe@gmail.com>
2021-10-19 16:16:41 +0900

    [MemorieDeFleurs] テストで参照する日付を含む、前後10日位を定数化

--- MemorieDeFleurs/MemorieDeFleursTest/ModelTest/DateConst.cs
+++ MemorieDeFleurs/MemorieDeFleursTest/ModelTest/DateConst.cs
@@ -7,18 +7,39 @@ namespace MemorieDeFleursTest.ModelTest
     {
         public static int Year = 2020;
 
+        public static DateTime April20th { get; } = new DateTime(Year, 4, 20);
+        public static DateTime April21st { get; } = new DateTime(Year, 4, 21);
+        public static DateTime April22nd { get; } = new DateTime(Year, 4, 22);
+        public static DateTime April23rd { get; } = new DateTime(Year, 4, 23);
+        public static DateTime April24th { get; } = new DateTime(Year, 4, 24);
+        public static DateTime April25th { get; } = new DateTime(Year, 4, 25);
+        public static DateTime April26th { get; } = new DateTime(Year, 4, 26);
+        public static DateTime April27th { get; } = new DateTime(Year, 4, 27);
+        public static DateTime April28th { get; } = new DateTime(Year, 4, 28);
+        public static DateTime April29th { get; } = new DateTime(Year, 4, 29);
+
         public static DateTime April30th { get; } = new DateTime(Year, 4, 30);
         public static DateTime May1st { get; } = new DateTime(Year, 5, 1);
         public static DateTime May2nd { get; } = new DateTime(Year, 5, 2);
-
         public static DateTime May3rd { get; } = new DateTime(Year, 5, 3);
-
         public static DateTime May4th { get; } = new DateTime(Year, 5, 4);
         public static DateTime May5th { get; } = new DateTime(Year, 5, 5);
         public static DateTime May6th { get; } = new DateTime(Year, 5, 6);
         public static DateTime May7th { get; } = new DateTime(Year, 5, 7);
         public static DateTime May8th { get; } = new DateTime(Year, 5, 8);
         public static DateTime May9th { get; } = new DateTime(Year, 5, 9);
+
+        public static DateTime May10th { get; } = new DateTime(Year, 5, 10);
+        public static DateTime May11th { get; } = new DateTime(Year, 5, 11);
+        public static DateTime May12th { get; } = new DateTime(Year, 5, 12);
+        public static DateTime May13th { get; } = new DateTime(Year, 5, 13);
+        public static DateTime May14th { get; } = new DateTime(Year, 5, 14);
+        public static DateTime May15th { get; } = new DateTime(Year, 5, 15);
+        public static DateTime May16th { get; } = new DateTime(Year, 5, 16);
+        public static DateTime May17th { get; } = new DateTime(Year, 5, 17);
+        public static DateTime May18th { get; } = new DateTime(Year, 5, 18);
+        public static DateTime May19th { get; } = new DateTime(Year, 5, 19);
+        public static DateTime May20th { get; } = new DateTime(Year, 5, 20);
 
     }
 }
