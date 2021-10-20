f7b0b04c3126787de2451e6ff375e0369447900c
Yasuhiro Tanabe <yasuhiro.tanabe@gmail.com>
2021-10-20 11:09:59 +0900

    [MemorieDeFleurs] メニューとメッセージ表示領域追加

--- MemorieDeFleurs/MemorieDeFleurs.UI.WPF/ViewModels/MainWindowViiewModel.cs
+++ MemorieDeFleurs/MemorieDeFleurs.UI.WPF/ViewModels/MainWindowViiewModel.cs
@@ -6,7 +6,27 @@ namespace MemorieDeFleurs.UI.WPF.ViewModels
 {
     public class MainWindowViiewModel : ViewModelBase
     {
-        public string WindowTitle { get; } = "Memorie de Fleurs 花卉受発注支援システム 管理画面";
+        #region プロパティ
+        /// <summary>
+        /// ウィンドウタイトル
+        /// </summary>
+        public string WindowTitle
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+        private string _title = "Memorie de Fleurs 花卉受発注支援システム 管理画面";
+
+        /// <summary>
+        /// システムメッセージ表示領域に表示する文言
+        /// </summary>
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+        private string _message;
+        #endregion // プロパティ
 
         #region コマンド
         public ICommand Exit { get; } = new ExitCommand();
