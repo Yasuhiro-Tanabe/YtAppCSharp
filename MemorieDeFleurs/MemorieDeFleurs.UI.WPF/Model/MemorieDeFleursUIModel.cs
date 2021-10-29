47c9c36631c80cf06f24b95b8520b0fc64dc37c8
Yasuhiro Tanabe <yasuhiro.tanabe@gmail.com>
2021-10-29 14:21:44 +0900

    [MemorieDeFleurs] UIモデルの公開メソッドにコメント追加、未使用のメソッド削除

--- MemorieDeFleurs/MemorieDeFleurs.UI.WPF/Model/MemorieDeFleursUIModel.cs
+++ MemorieDeFleurs/MemorieDeFleurs.UI.WPF/Model/MemorieDeFleursUIModel.cs
@@ -18,6 +18,9 @@ namespace MemorieDeFleurs.UI.WPF.Model
 
         private MemorieDeFleursUIModel() { }
 
+        /// <summary>
+        /// UI用モデルのインスタンス
+        /// </summary>
         public static MemorieDeFleursUIModel Instance { get { return _singleton; } }
 
         /// <summary>
@@ -33,6 +36,10 @@ namespace MemorieDeFleurs.UI.WPF.Model
         private MemorieDeFleursModel Model { get; set; }
 
         #region データベースの操作
+        /// <summary>
+        /// SQLite データベースファイルをロードし接続する
+        /// </summary>
+        /// <param name="path">ロードする SQLite データベースファイル</param>
         public void OpenSQLiteDatabaseFile(string path)
         {
             LogUtil.DEBUGLOG_BeginMethod(path);
@@ -53,6 +60,10 @@ namespace MemorieDeFleurs.UI.WPF.Model
             }
         }
 
+        /// <summary>
+        /// 接続中の SQLite データベースの内容をを指定ファイルに保存する
+        /// </summary>
+        /// <param name="path">保存するデータベースファイル名</param>
         public void SaveSQLiteConnectinToFile(string path)
         {
             LogUtil.DEBUGLOG_BeginMethod(path);
@@ -88,18 +99,31 @@ namespace MemorieDeFleurs.UI.WPF.Model
         #endregion // データベースの操作
 
         #region 単品の操作
+        /// <summary>
+        /// 接続中のデータベースに登録されている単品をすべて取得する
+        /// </summary>
+        /// <returns>登録されている単品のコレクション、単品が登録されていないときは空のコレクション</returns>
         public IEnumerable<BouquetPart> FindAllBouquetParts()
         {
             if (DbConnection == null) { throw new NotConnectedToDatabaseException(); }
             return Model.BouquetModel.FindAllBoueuqtParts();
         }
 
+        /// <summary>
+        /// 接続中のデータベースに登録されている単品を取得する
+        /// </summary>
+        /// <param name="partsCode">花コード</param>
+        /// <returns>花コードに該当する単品</returns>
         public BouquetPart FindBouquetParts(string partsCode)
         {
             if (DbConnection == null) { throw new NotConnectedToDatabaseException(); }
             return Model.BouquetModel.FindBouquetPart(partsCode);
         }
 
+        /// <summary>
+        /// 接続中のデータベースに登録されている単品を削除する
+        /// </summary>
+        /// <param name="partsCode">花コード</param>
         public void RemoveBouquetParts(string partsCode)
         {
             if (DbConnection == null) { throw new NotConnectedToDatabaseException(); }
@@ -108,58 +132,68 @@ namespace MemorieDeFleurs.UI.WPF.Model
         #endregion // 単品の操作
 
         #region 商品と商品構成の操作
+        /// <summary>
+        /// 接続中のデータベースに登録されている商品をすべて取得する
+        /// </summary>
+        /// <returns>登録されている商品のコレクション、商品が登録されていないときは空のコレクション</returns>
         public IEnumerable<Bouquet> FindAllBouquets()
         {
             if (DbConnection == null) { throw new NotConnectedToDatabaseException(); }
             return Model.BouquetModel.FindAllBouquets();
         }
 
+        /// <summary>
+        /// 接続中のデータベースに登録されている商品を取得する
+        /// </summary>
+        /// <param name="bouquetCode">花束コード</param>
         public void RemoveBouquet(string bouquetCode)
         {
             if (DbConnection == null) { throw new NotConnectedToDatabaseException(); }
             Model.BouquetModel.RemoveBouquet(bouquetCode);
         }
 
+        /// <summary>
+        /// 接続中のデータベースに登録されている商品を削除する
+        /// </summary>
+        /// <param name="bouquetCode">花束コード</param>
+        /// <returns></returns>
         public Bouquet FindBouquet(string bouquetCode)
         {
             if (DbConnection == null) { throw new NotConnectedToDatabaseException(); }
             return Model.BouquetModel.FindBouquet(bouquetCode);
         }
-
-        public void UpdatePartsList(string bouquetCode, IEnumerable<KeyValuePair<string, int>> partsList)
-        {
-            if (DbConnection == null) { throw new NotConnectedToDatabaseException(); }
-            Model.BouquetModel.RemoveAllPartsFrom(bouquetCode);
-            foreach(var kv in partsList)
-            {
-                Model.BouquetModel.UpdateQuantityOf(bouquetCode, kv.Key, kv.Value);
-            }
-        }
         #endregion // 商品と商品構成の操作
 
         #region 仕入先と単品仕入先の操作
+        /// <summary>
+        /// 接続中のデータベースに登録されている仕入先をすべて取得する
+        /// </summary>
+        /// <returns>登録されている仕入先のコレクション、仕入先が登録されていないときは空のコレクション</returns>
         public IEnumerable<Supplier> FindAllSuppliers()
         {
             if (DbConnection == null) { throw new NotConnectedToDatabaseException(); }
             return Model.SupplierModel.FindAllSuppliers();
         }
 
+        /// <summary>
+        /// 接続中のデータベースに登録されている仕入先を取得する
+        /// </summary>
+        /// <param name="supplierCode">仕入先ID</param>
+        /// <returns></returns>
         public Supplier FindSupplier(int supplierCode)
         {
             if (DbConnection == null) { throw new NotConnectedToDatabaseException(); }
             return Model.SupplierModel.FindSupplier(supplierCode);
         }
 
+        /// <summary>
+        /// 接続中のデータベースから仕入先を削除する
+        /// </summary>
+        /// <param name="supplierCode">仕入先ID</param>
         public void RemoveSupplier(int supplierCode)
         {
             if (DbConnection == null) { throw new NotConnectedToDatabaseException(); }
             Model.SupplierModel.RemoveSupplier(supplierCode);
-        }
-
-        public void UpdateSupplingParts(int supplierCode, IEnumerable<string> parts)
-        {
-            if (DbConnection == null) { throw new NotConnectedToDatabaseException(); }
-            throw new NotImplementedException();
         }
         #endregion // 仕入先と単品仕入先の操作
 
