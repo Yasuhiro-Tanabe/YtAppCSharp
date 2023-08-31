33b0e85f5cc1b334720e026a43f2587f26a1bd3d
Yasuhiro Tanabe <yasuhiro.tanabe@gmail.com>
2023-09-01 08:02:50 +0900

    [Framework] WPF1.0.3 - 自動スクロールができていなかったバグの修正

--- YasT.Framework/YasT.Framework.WPF/ScrollViewerHelper.cs
+++ YasT.Framework/YasT.Framework.WPF/ScrollViewerHelper.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
 namespace YasT.Framework.WPF
 {
@@ -8,7 +10,7 @@ namespace YasT.Framework.WPF
     /// </summary>
     public static class ScrollViewerHelper
     {
-        #region AutoScroll プロパティ
+        #region AutoScrollToBottom プロパティ
         /// <summary>
         /// <see cref="AutoScrollToBottom"/> プロパティの値を取得する
         /// </summary>
@@ -32,7 +34,7 @@ namespace YasT.Framework.WPF
         /// <summary>
         /// 自動的に最下部までスクロールするかどうか
         /// 
-        /// <see cref="TextBlock"/> や <see cref="TextBox"/> を囲う <see cref="ScrollViewer"/> に添付する。
+        /// <see cref="TextBlock"/> や <see cref="TextBox"/>、<see cref="ScrollViewer"/> に添付する。
         /// 真のときテキストブロックの内容変更に合わせて随時テキストの末尾が表示されるように移動する
         /// </summary>
         public static readonly DependencyProperty AutoScrollToBottom =
@@ -48,9 +50,46 @@ namespace YasT.Framework.WPF
         /// <param name="e">イベントパラメータ</param>
         private static void AutoScrollToBottomPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            if(d is ScrollViewer && (bool)e.NewValue)
+            if(d is ScrollViewer s)
             {
-                (d as ScrollViewer)?.ScrollToBottom();
+                if((bool)e.NewValue)
+                {
+                    s.ScrollChanged += OnScrollChanged;
+                }
+                else
+                {
+                    s.ScrollChanged -= OnScrollChanged;
+                }
+            }
+            else if(d is TextBoxBase b)
+            {
+                if((bool)e.NewValue)
+                {
+                    b.TextChanged += OnTextChanged;
+                }
+                else
+                {
+                    b.TextChanged -= OnTextChanged;
+                }
+            }
+        }
+        private static void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender == null) { return; }
+            if(sender is TextBoxBase b)
+            {
+                b.ScrollToEnd();
+            }
+        }
+        private static void OnScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            if(sender == null) { return; }
+            if(sender is ScrollViewer s)
+            {
+                if(e.ExtentHeightChange < -1.0 || 1.0 < e.ExtentHeightChange)
+                {
+                    s.ScrollToBottom();
+                }
             }
         }
         #endregion
